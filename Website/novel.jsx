0285ba73c632c1297f51f802c087cac87bc63f6b
Yulan-Zhu <70460662+Yulan-Zhu@users.noreply.github.com>
2020-11-02 13:21:52 +0800

    Add files via upload

---
+++ Website/novel.jsx
@@ -0,0 +1,6 @@
+function table(){
+    var selecter = parent.document.getElementById("selecter");
+                                if (selecter.value=="0"){document.getElementById("iframe").src="https://airtable.com/embed/shrxBIzUpkVT9YnnP?backgroundColor=cyanLight&viewControls=on"};
+                                if (selecter.value=="1"){document.getElementById("iframe").src="https://airtable.com/embed/shrCi0nYDcrdLy2Ve?backgroundColor=cyanLight&viewControls=on"};
+                                if (selecter.value=="2"){document.getElementById("iframe").src="https://airtable.com/embed/shr4HZ5gyX2JYgWCg?backgroundColor=cyanLight&viewControls=on"};
+                                }
